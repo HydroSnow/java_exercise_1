d6ef1ffcaaee830b2bef6c8257cd2ece6bbdf73a
Alexis Trupin <link9745@gmail.com>
2021-11-29 14:32:06 +0100

    adding fibonnaci

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,6 +1,18 @@
 import java.util.Scanner;
 
 public class Launcher {
+    public static int fibo(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("N must be positive");
+        } else if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibo(n - 1) + fibo(n - 2);
+        }
+    }
+
     public static void main(final String[] args) {
         System.out.println("Bienvenue");
         final Scanner scanner = new Scanner(System.in);
@@ -9,6 +21,14 @@ public class Launcher {
             final String str = scanner.nextLine();
             if (str.equalsIgnoreCase("quit")) {
                 running = false;
+            } else if (str.equalsIgnoreCase("fibo")) {
+                System.out.println("Entrez un nombre");
+                final int n = scanner.nextInt();
+                // consommer le caractère de fin de ligne
+                scanner.nextLine();
+
+                final int result = fibo(n);
+                System.out.println(result);
             } else {
                 System.out.println("Unknown command");
             }
